1ef4236a6285bc6718089a76615503fd81d5deec
Genaro Figueiredo <genarofigueiredo@hotmail.com>
2020-03-16 18:48:16 -0300

    added in to agendas table.

---
+++ database/migrations/2020_03_16_214629_create_agendas_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAgendasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('agendas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('pacient_id')->unsigned();
+            $table->foreign('pacient_id')->references('id')->on('pacients');
+            $table->integer('professional_id')->unsigned();
+            $table->foreign('professional_id')->references('id')->on('professionals');
+            $table->dateTime('date');
+            $table->dateTime('duration');
+            $table->string('details')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('agendas');
+    }
+}
